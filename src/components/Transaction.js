ee55386a3412dd70f7bda72591cea9dd2737c04b
Yash Shukla <yashshukla1912@gmail.com>
2020-12-28 23:56:31 +0530

    Created new component Transaction

---
+++ src/components/Transaction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Transaction() {
+    return (
+        <>
+            <li className="minus">
+                Cash <span>-$400</span><button className="delete-btn">x</button>
+            </li>
+        </>
+    )
+}
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,17 +1,16 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
+import Transaction from './Transaction'
 
 export default function TransactionList() {
-    const context = useContext(GlobalContext)
-    console.log(context)
+    const transactions = useContext(GlobalContext) || []
+    console.log(transactions)
 
     return (
         <>
             <h3>History</h3>
             <ul className="list">
-                <li className="minus">
-                    Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>
+                { transactions.map(transaction => <Transaction transaction={transaction}/>)  }
             </ul>
         </>
     )
